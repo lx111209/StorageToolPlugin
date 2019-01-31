163af54b5df25575300f79b0bf116cb04e431933
lixiang <lixiang@winchannel.net>
2019-01-31 16:07:29 +0800

    上传

--- demo.js
+++ demo.js
@@ -9,19 +9,3 @@ defineClass("ViewController", {
             console.log("456");
             }
             },{})
-
-
-// demo.js
-//require('UIView, UIColor, UILabel')
-//defineClass('AppDelegate', {
-//            // replace the -genView method
-//            genView: function() {
-//            var view = self.ORIGgenView();
-//            view.setBackgroundColor(UIColor.greenColor())
-//            var label = UILabel.alloc().initWithFrame(view.frame());
-//            label.setText("JSPatch");
-//            label.setTextAlignment(1);
-//            view.addSubview(label);
-//            return view;
-//            }
-//            });

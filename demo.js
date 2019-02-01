4d6575ca7484fb3167863f364579d10e0673c0e6
lixiang <lixiang@winchannel.net>
2019-02-01 10:13:37 +0800

    Xiugai

--- demo.js
+++ demo.js
@@ -1,4 +1,13 @@
 require("UIImage,NSData,NSArray,NSMutableDictionary,NSMutableString,NSURL")
+
+defineClass("ViewController", {
+            pushSecondVC:function(sender) {
+                console.log("onClick");
+                var vc = SecondViewController.alloc().init();
+        		self.navigationController().pushViewController_animated(vc, YES);
+            }
+            },{})
+
 defineClass("SecondViewController", {
             viewDidLoad:function() {
             self.ORIGviewDidLoad();
